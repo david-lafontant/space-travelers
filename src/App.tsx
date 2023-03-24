bf641c142226ff19554527a5ce89889b96d0856f
david-lafontant <lafontant.david@gmail.com>
2023-03-24 15:06:13 -0600

    Display missions page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from "./components/header/Header";
+import Missions from "./pages/missions/Missions";
 import Rockets from "./pages/rockets/Rockets";
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Rockets />
+      <Missions />
     </div>
   )
 }
