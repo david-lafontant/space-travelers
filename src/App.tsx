b12ae8127395082f5d9b7046dbdd5b27b17c40a3
david-lafontant <lafontant.david@gmail.com>
2023-03-24 15:04:12 -0600

    Display Rockets page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
+import Rockets from "./pages/rockets/Rockets";
 function App() {
 
 
   return (
     <div className="App">
       <Header />
+      <Rockets />
     </div>
   )
 }
