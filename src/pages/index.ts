61350768baaa1e7f890b79e1813e1aeb43126b85
david-lafontant <lafontant.david@gmail.com>
2023-03-24 15:49:25 -0600

    Implement barrel export pattern

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import Dragons from './dragons/Dragons';
+import Missions from './missions/Missions';
+import Rockets from './rockets/Rockets';
+import Profile from './profile/Profile';
+export { Dragons, Missions, Rockets, Profile };
