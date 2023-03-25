892f433f53a7c3a55504f3542b9a05a92d477a37
david-lafontant <lafontant.david@gmail.com>
2023-03-25 16:24:07 -0600

    Display Mission component in Mission page

--- src/pages/missions/Missions.tsx
+++ src/pages/missions/Missions.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import Mission from '../../components/mission/Mission';
 
 const Missions = () => {
   return (
-    <div>Missions</div>
+    <article className="row">
+      <div className="col">
+        <div className="p-4">
+
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Mission</th>
+                <th scope="col">Description</th>
+                <th scope="col">Status</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <Mission />
+              <Mission />
+              <Mission />
+              <Mission />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </article>
   )
 }
 
