8df8ce57c9106e9d795b8f10c8dd5aa63f78593a
david-lafontant <lafontant.david@gmail.com>
2023-03-25 13:05:44 -0600

    Create a ButtonComponent

---
+++ src/components/button/ButtonComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const ButtonComponent = () => {
+  return (
+    <Button className="btn btn-primary">My Button</Button>
+  )
+}
+
+export default ButtonComponent;
