01e5a0d713db02601762530b308e08f0fbbeccb8
david-lafontant <lafontant.david@gmail.com>
2023-03-25 16:47:30 -0600

    Display Info component in Profile page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React from "react";
+import Info from "../../components/info/Info";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <article className="row">
+      <div className="col-md-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">My Mission</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Info />
+            <Info />
+            <Info />
+          </tbody>
+        </table>
+      </div>
+      <div className="col-md-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">My Rockets</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Info />
+            <Info />
+            <Info />
+          </tbody>
+        </table>
+      </div>
+      <div className="col-md-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">My Dragons</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Info />
+            <Info />
+            <Info />
+          </tbody>
+        </table>
+      </div>
+    </article>
+  );
+};
 
 export default Profile;
