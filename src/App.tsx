b8966a6d69f61ad5b0bbfe85d770e9756c00472a
david-lafontant <lafontant.david@gmail.com>
2023-03-25 12:18:55 -0600

    Refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Header from "./components/header/Header";
 import { Dragons, Missions, Profile, Rockets } from './pages/index';
 
+const ROUTER = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Header />}>
+      <Route index element={<Rockets />} />
+      <Route exact path="/missions" element={<Missions />} />
+      <Route exact path="/dragons" element={<Dragons />} />
+      <Route exact path="/profile" element={<Profile />} />
+    </Route>
+  )
+);
 function App() {
   return (
     <div className="App">
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/dragons" element={<Dragons />} />
-          <Route path="/Missions" element={<Missions />} />
-          <Route path="profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
-
-
-
+      <RouterProvider router={ROUTER} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
