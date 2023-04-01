c9620bf10f4928e5775a4785ed44c6bbd36ecee5
david-lafontant <lafontant.david@gmail.com>
2023-04-01 16:26:38 -0600

    Pass value retrieved as props to Rocket

--- src/pages/rockets/Rockets.tsx
+++ src/pages/rockets/Rockets.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Rocket } from '../../components/rocket/Rocket';
+import React from "react";
+import { useGetRocketsQuery } from "../../features/rockets/rocketSlice";
+import { Rocket } from "../../components/rocket/Rocket";
 
-const Rockets = () => (
-  <section className="container-fluid">
-    Rockets
-    <Rocket />
-    <Rocket />
-    <Rocket />
-  </section>
-);
+const Rockets = () => {
+  const { data, isLoading, error } = useGetRocketsQuery(null);
+  if (isLoading) {
+    return <div>Loading posts...</div>;
+  }
+
+  if (error) {
+    return <h1>ERROR</h1>;
+  }
+
+  return (
+    <section className="container-fluid">
+      {data?.map((rocket) => (
+        <Rocket key={rocket.id} {...rocket} />
+      ))}
+    </section>
+  );
+};
 
 export default Rockets;
