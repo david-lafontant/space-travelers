1c5b603868099b425b00e37d74eb07decf782857
david-lafontant <lafontant.david@gmail.com>
2023-04-01 16:25:52 -0600

    Display rocket image, name , and description

--- src/components/rocket/Rocket.tsx
+++ src/components/rocket/Rocket.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, { FC } from 'react';
 import ButtonComponent from '../button/ButtonComponent';
-import rocket from '../../assets/images/rockets.jpg';
+import { ARocket } from '../../types/rocketsTypes';
 
-export const Rocket = () => (
-  <article className="row">
-    <div className="fill col-md-4 ">
-      <img src={rocket} className="img-fluid w-100" alt="rocket" />
-    </div>
-    <div className="col-md-8 ">
-      <div className="d-flex flex-column justify-content-around align-items-start">
+export const Rocket = ({ rocket_name, description, flickr_images }: ARocket) => {
 
-        <h2>The Rockets</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-          quos quasi repudiandae similique ullam at. Velit similique rem
-          harum aut eveniet illum repellat accusantium ducimus corporis,
-          magnam quas ad placeat!
-        </p>
-        <ButtonComponent />
+  return (
+    <article className="row">
+      <div className="fill col-md-4 ">
+        // <img src={flickr_images[0]} className="img-fluid w-100" alt="rocket" />
       </div>
-    </div>
+      <div className="col-md-8 ">
+        <div className="d-flex flex-column justify-content-around align-items-start">
 
-  </article>
-);
+          <h2>{rocket_name}</h2>
+          <p>
+            {description}
+          </p>
+          <ButtonComponent />
+        </div>
+      </div>
+
+    </article>
+  )
+};
 
 export default Rocket;
