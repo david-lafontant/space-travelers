6e8549f78fd972c76ac119a9aa3d5a61692a6beb
david-lafontant <lafontant.david@gmail.com>
2023-04-01 16:29:00 -0600

    Remove // and add rocket name as image alt

--- src/components/rocket/Rocket.tsx
+++ src/components/rocket/Rocket.tsx
@@ -7,7 +7,7 @@ export const Rocket = ({ rocket_name, description, flickr_images }: ARocket) => 
   return (
     <article className="row">
       <div className="fill col-md-4 ">
-        // <img src={flickr_images[0]} className="img-fluid w-100" alt="rocket" />
+        <img src={flickr_images[0]} className="img-fluid w-100" alt={rocket_name} />
       </div>
       <div className="col-md-8 ">
         <div className="d-flex flex-column justify-content-around align-items-start">
