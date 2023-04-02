de1ec9c83ddbb3c2e7c00a54d9638837ddfb21e7
david-lafontant <lafontant.david@gmail.com>
2023-04-02 08:22:31 -0600

    Add any type to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rocketService } from '../services/rocket/rocketService';
+import { dragonService } from '../services/dragon/dragonService';
+import { missionService } from '../services/mission/missionService';
 
-export const store = configureStore({
+export const store:any = configureStore({
   reducer: {
     [rocketService.reducerPath]: rocketService.reducer,
+    [dragonService.reducerPath]: dragonService.reducer,
+    [missionService.reducerPath]: missionService.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(rocketService.middleware),
+    getDefaultMiddleware().concat(rocketService.middleware, dragonService.middleware, missionService.middleware),
 });
