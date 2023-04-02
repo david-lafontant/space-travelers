2f44192015c30108bd37d42692359243ef88ab29
david-lafontant <lafontant.david@gmail.com>
2023-04-02 08:20:26 -0600

    Retrieve dragons data and pass it as props to Dragon component

--- src/pages/dragons/Dragons.tsx
+++ src/pages/dragons/Dragons.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useGetDragonsQuery } from '../../services/dragon/dragonService';
+import { ADragon } from '../../types/types';
 import Dragon from '../../components/dragon/Dragon';
 
-const Dragons = () => (
-  <section className="container-fluid">
-    Rockets
-    <Dragon />
-    <Dragon />
-    <Dragon />
-  </section>
-);
+const Dragons = () => {
+  const { data, isLoading, error } = useGetDragonsQuery(null);
+  if (isLoading) {
+    return <div>Loading Dragons...</div>;
+  }
+
+  if (error) {
+    return <h1>ERROR</h1>;
+  }
+  return (
+    <section className="container-fluid">
+      {data?.map((dragon: JSX.IntrinsicAttributes & ADragon) => (
+        <Dragon key={dragon.id} {...dragon} />
+      ))}
+    </section>
+  )
+};
 
 export default Dragons;
