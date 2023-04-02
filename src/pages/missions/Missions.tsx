cf24e0a6fb9c668f865c67c2f36192977388808b
david-lafontant <lafontant.david@gmail.com>
2023-04-02 08:19:02 -0600

    Retrieve mission data and pass it as props to Mission component

--- src/pages/missions/Missions.tsx
+++ src/pages/missions/Missions.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
+import { useGetMissionsQuery } from '../../services/mission/missionService';
 import Mission from '../../components/mission/Mission';
+import { AMission } from '../../types/types';
 
-const Missions = () => (
-  <article className="row">
-    <div className="col">
-      <div className="p-4">
+const Missions = () => {
+  const { data, isLoading, error } = useGetMissionsQuery(null);
+  if (isLoading) {
+    return <div>Loading Mission...</div>;
+  }
 
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Mission</th>
-              <th scope="col">Description</th>
-              <th scope="col">Status</th>
-              <th scope="col">blank</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Mission />
-            <Mission />
-            <Mission />
-            <Mission />
-          </tbody>
-        </table>
+  if (error) {
+    return <h1>ERROR</h1>;
+  }
+  return (
+    <article className="row">
+      <div className="col">
+        <div className="p-4">
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Mission</th>
+                <th scope="col">Description</th>
+                <th scope="col">Status</th>
+                <th scope="col">blank</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {data?.map((mission: JSX.IntrinsicAttributes & AMission) => (
+                <Mission key={mission.mission_id} {...mission} />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  </article>
-);
+    </article>
+  )
+};
 
 export default Missions;
