b21c5258c2f2abdc8a7d43825d36eecb26601e06
david-lafontant <lafontant.david@gmail.com>
2023-04-12 22:25:12 -0600

    Conditional rendering of button component in Dragon

--- src/components/dragon/Dragon.tsx
+++ src/components/dragon/Dragon.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import ButtonComponent from '../button/ButtonComponent';
-import { ADragon } from '../../types/types';
+import ButtonComponent from '../button/ButtonComponent'
+import { ADragon } from '../../types/types'
 
-const Dragon = ({ name, description, flickr_images }: ADragon) => (
-  <article className="row">
-    <div className="fill col-md-4 ">
-      <img src={flickr_images[0]} className="img-fluid w-100" alt="dragon" />
-    </div>
-    <div className="col-md-8 ">
-      <div className="d-flex flex-column justify-content-around align-items-start">
-        <h2>{name}</h2>
-        <p>{description}</p>
-        <ButtonComponent />
+const Dragon = ({ name, description, flickr_images, booked, id }: ADragon) => {
+  const onClick = (id: number | string) => {
+    console.log(`dragon id = ${id}`)
+  }
+  return (
+    <article className="row">
+      <div className="fill col-md-4 ">
+        <img src={flickr_images[0]} className="img-fluid w-100" alt="dragon" />
       </div>
-    </div>
-  </article>
-);
+      <div className="col-md-8 ">
+        <div className="d-flex flex-column justify-content-around align-items-start">
+          <h2>{name}</h2>
+          <p>{description}</p>
+          {booked ? (
+            <ButtonComponent
+              booked={booked}
+              text="Cancel Reservation"
+              id={id}
+              onClick={onClick}
+            />
+          ) : (
+            <ButtonComponent
+              booked={booked}
+              text="Reserve Rocket"
+              id={id}
+              onClick={onClick}
+            />
+          )}
+        </div>
+      </div>
+    </article>
+  )
+}
 
-export default Dragon;
+export default Dragon
