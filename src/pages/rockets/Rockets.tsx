77e005c0704ed27d5a83ec0078cb4b70857a73f4
david-lafontant <lafontant.david@gmail.com>
2023-04-12 21:14:58 -0600

    Display spinner in rockets' page

--- src/pages/rockets/Rockets.tsx
+++ src/pages/rockets/Rockets.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { getRockets } from '../../features/rocket/rocketSLice'
 import { useAppDispatch, useAppSelector } from '../../hooks/useTypedSelectors'
 import Rocket from '../../components/rocket/Rocket'
+import Spinner from '../../components/spinner/Spinner'
 import { ARocket } from '../../types/types'
 
 const Rockets = () => {
@@ -15,9 +16,14 @@ const Rockets = () => {
 
   return (
     <section className="container-fluid">
-      {data?.map((rocket: JSX.IntrinsicAttributes & ARocket) => (
-        <Rocket key={rocket.id} {...rocket} />
-      ))}
+      {loading ? (
+        <Spinner />
+      ) : (
+        data &&
+        data.map((rocket: JSX.IntrinsicAttributes & ARocket) => (
+          <Rocket key={rocket.id} {...rocket} />
+        ))
+      )}
     </section>
   )
 }
