2dbad9f0638295d7b50605e491edcf803c80e6bc
david-lafontant <lafontant.david@gmail.com>
2023-04-12 22:25:49 -0600

    Conditional rendering of Button component in Mission

--- src/components/mission/Mission.tsx
+++ src/components/mission/Mission.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import ButtonComponent from '../button/ButtonComponent';
-import { AMission } from '../../types/types';
+import ButtonComponent from '../button/ButtonComponent'
+import { AMission } from '../../types/types'
 
-const Mission = ({ mission_name, description }: AMission) => (
-  <tr>
-    <th scope="row">{mission_name}</th>
-    <td>
-      {description}
-    </td>
-    <td>
-      <p className="badge bg-info text-dark">
-        Status badge
-      </p>
-    </td>
-    <td>
-      <ButtonComponent />
-    </td>
-  </tr>
-);
+const Mission = ({
+  mission_name,
+  description,
+  booked,
+  mission_id,
+}: AMission) => {
+  const onClick = (id: number | string) => {
+    console.log(`mission id = ${id}`)
+  }
+  return (
+    <tr>
+      <th scope="row">{mission_name}</th>
+      <td>{description}</td>
+      <td>
+        <p className="badge bg-info text-dark">Status badge</p>
+      </td>
+      <td>
+        {booked ? (
+          <ButtonComponent
+            booked={booked}
+            text="Cancel Reservation"
+            id={mission_id}
+            onClick={onClick}
+          />
+        ) : (
+          <ButtonComponent
+            booked={booked}
+            text="Reserve Rocket"
+            id={mission_id}
+            onClick={onClick}
+          />
+        )}
+      </td>
+    </tr>
+  )
+}
 
-export default Mission;
+export default Mission
