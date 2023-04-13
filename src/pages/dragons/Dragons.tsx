4ca9f94047297d8fc057026beb6ab66d5a5a0510
david-lafontant <lafontant.david@gmail.com>
2023-04-12 19:02:18 -0600

    Remove reference to services in Dragons and Missions

--- src/pages/dragons/Dragons.tsx
+++ src/pages/dragons/Dragons.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import { useGetDragonsQuery } from '../../services/dragon/dragonService';
 import { ADragon } from '../../types/types';
 import Dragon from '../../components/dragon/Dragon';
 
 const Dragons = () => {
-  const { data, isLoading, error } = useGetDragonsQuery(null);
-  if (isLoading) {
-    return <div>Loading Dragons...</div>;
-  }
 
-  if (error) {
-    return <h1>ERROR</h1>;
-  }
   return (
     <section className="container-fluid">
-      {data?.map((dragon: JSX.IntrinsicAttributes & ADragon) => (
+      {/* {data?.map((dragon: JSX.IntrinsicAttributes & ADragon) => (
         <Dragon key={dragon.id} {...dragon} />
-      ))}
+      ))} */}
     </section>
   );
 };
--- src/pages/missions/Missions.tsx
+++ src/pages/missions/Missions.tsx
@@ -1,22 +1,8 @@
-import React from 'react';
-import { useGetMissionsQuery } from '../../services/mission/missionService';
-import Mission from '../../components/mission/Mission';
-import { AMission } from '../../types/types';
-
 const Missions = () => {
-  const { data, isLoading, error } = useGetMissionsQuery(null);
-  if (isLoading) {
-    return <div>Loading Mission...</div>;
-  }
-
-  if (error) {
-    return <h1>ERROR</h1>;
-  }
   return (
     <article className="row">
       <div className="col">
         <div className="p-4">
-
           <table className="table">
             <thead>
               <tr>
@@ -27,16 +13,15 @@ const Missions = () => {
               </tr>
             </thead>
             <tbody>
-
-              {data?.map((mission: JSX.IntrinsicAttributes & AMission) => (
+              {/* {data?.map((mission: JSX.IntrinsicAttributes & AMission) => (
                 <Mission key={mission.mission_id} {...mission} />
-              ))}
+              ))} */}
             </tbody>
           </table>
         </div>
       </div>
     </article>
-  );
-};
+  )
+}
 
-export default Missions;
+export default Missions
