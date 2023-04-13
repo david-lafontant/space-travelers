dfb7a79a87e45228805af1633848bf1b5e338fd9
david-lafontant <lafontant.david@gmail.com>
2023-04-12 20:30:01 -0600

    Display dragons

--- src/pages/dragons/Dragons.tsx
+++ src/pages/dragons/Dragons.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { ADragon } from '../../types/types';
-import Dragon from '../../components/dragon/Dragon';
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../hooks/useTypedSelectors'
+import { ADragon } from '../../types/types'
+import Dragon from '../../components/dragon/Dragon'
+import { getDragons } from '../../features/dragon/dragonSlice'
 
 const Dragons = () => {
+  const dispatch = useAppDispatch()
 
+  useEffect(() => {
+    dispatch(getDragons())
+  }, [dispatch])
+
+  let { loading, data } = useAppSelector((state) => state.dragons)
+  data?.forEach((item) => console.log(item.booked))
   return (
     <section className="container-fluid">
-      {/* {data?.map((dragon: JSX.IntrinsicAttributes & ADragon) => (
+      {data?.map((dragon: JSX.IntrinsicAttributes & ADragon) => (
         <Dragon key={dragon.id} {...dragon} />
-      ))} */}
+      ))}
     </section>
-  );
-};
+  )
+}
 
-export default Dragons;
+export default Dragons
