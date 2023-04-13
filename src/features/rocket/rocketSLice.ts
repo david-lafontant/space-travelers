84c90c5e497b3d3dd7eb92ce5267d8d1d5187417
david-lafontant <lafontant.david@gmail.com>
2023-04-12 19:02:36 -0600

    Setup rocket slice

---
+++ src/features/rocket/rocketSLice.ts
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+import { RocketType } from '../../types/types'
+
+export const getRockets = createAsyncThunk(
+  'rockets/getRockets',
+  async (data, thunkApi) => {
+    try {
+      const response = await axios.get<RocketType>(
+        'https://api.spacexdata.com/v4/rockets'
+      )
+      return response.data
+    } catch (error: any) {
+      const message = error.message
+      return thunkApi.rejectWithValue(message)
+    }
+  }
+)
+
+interface RocketState {
+  loading: boolean
+  error: string | null
+  data: RocketType | null
+}
+
+const initialState = {
+  loading: false,
+  error: null,
+  data: null,
+} as RocketState
+
+const rocketSlice = createSlice({
+  name: 'rocket',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getRockets.pending, (state, action) => {
+        state.loading = true
+      })
+      .addCase(
+        getRockets.fulfilled,
+        (state, action: PayloadAction<RocketType>) => {
+          state.loading = false
+          state.data = action.payload
+        }
+      )
+      .addCase(getRockets.rejected, (state, action: PayloadAction<any>) => {
+        state.error = action.payload
+      })
+  },
+})
+
+export default rocketSlice.reducer
