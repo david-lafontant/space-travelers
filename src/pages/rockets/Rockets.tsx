ea664489d0281a95dd96ff9e3d3cf86b103adac5
david-lafontant <lafontant.david@gmail.com>
2023-04-12 19:04:10 -0600

    Remove reference to rocekt service - display rocket from state

--- src/pages/rockets/Rockets.tsx
+++ src/pages/rockets/Rockets.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { useGetRocketsQuery } from '../../services/rocket/rocketService';
-import { Rocket } from '../../components/rocket/Rocket';
-import { ARocket } from '../../types/types';
+import { useEffect } from 'react'
+// import { useGetRocketsQuery } from '../../services/rocket/rocketService';
+import { getRockets } from '../../features/rocket/rocketSLice'
+import { useAppDispatch, useAppSelector } from '../../hooks/useTypedSelectors'
+import Rocket from '../../components/rocket/Rocket'
+import { ARocket } from '../../types/types'
 
 const Rockets = () => {
-  const { data, isLoading, error } = useGetRocketsQuery(null);
-  if (isLoading) {
-    return <div>Loading rockets...</div>;
-  }
+  // const { data, isLoading, error } = useGetRocketsQuery(null);
+  // if (isLoading) {
+  //   return <div>Loading rockets...</div>;
+  // }
 
-  if (error) {
-    return <h1>ERROR</h1>;
-  }
+  // if (error) {
+  //   return <h1>ERROR</h1>;
+  // }
+  const dispatch = useAppDispatch()
 
+  useEffect(() => {
+    dispatch(getRockets())
+  }, [dispatch])
+
+  let { loading, data } = useAppSelector((state) => state)
+  
   return (
     <section className="container-fluid">
       {data?.map((rocket: JSX.IntrinsicAttributes & ARocket) => (
         <Rocket key={rocket.id} {...rocket} />
       ))}
     </section>
-  );
-};
+  )
+}
 
-export default Rockets;
+export default Rockets
