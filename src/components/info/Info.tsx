69563b23bfd78b6dc643475e82b708771f531256
david-lafontant <lafontant.david@gmail.com>
2023-04-17 22:04:31 -0600

    Display cancel button in my profil page for booked rockets, dragons, and missions

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,11 +1,48 @@
+import { useAppDispatch } from '../../hooks/useTypedSelectors'
+import { toggleBooking as dragonBooking } from '../../features/dragon/dragonSlice'
+import { toggleBooking as missionBooking } from '../../features/mission/missionSlice'
+import { toggleBooking as rocketBooking } from '../../features/rocket/rocketSLice'
 type InfoProps = {
   name: string
+  category: 'mission' | 'rocket' | 'dragon'
+  id: number | string
 }
 
-const Info = ({ name }: InfoProps) => (
-  <tr>
-    <td>{name}</td>
-  </tr>
-);
+const Info = ({ name, category, id }: InfoProps) => {
+  const dispatch = useAppDispatch()
+  const handleClick = (
+    id: number | string,
+    category: 'mission' | 'rocket' | 'dragon'
+  ) => {
+    switch (category) {
+      case 'mission':
+        dispatch(missionBooking(id))
+        break
+      case 'rocket':
+        dispatch(rocketBooking(id))
+        break
+      case 'dragon':
+        dispatch(dragonBooking(id))
+        break
+      default:
+        const error = 'error'
+        break
+    }
+    console.log('function fired')
+  }
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>
+        <button
+          onClick={() => handleClick(id, category)}
+          className="btn btn-outline-secondary"
+        >
+          Cancel Reservation
+        </button>
+      </td>
+    </tr>
+  )
+}
 
-export default Info;
+export default Info
