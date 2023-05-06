1024a0e43fb8d92393ee77cc1d8475e35d275192
david-lafontant <lafontant.david@gmail.com>
2023-05-06 11:18:05 -0600

    Add error boundary to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ErrorBoundary from './components/error/ErrorBoundary'
 import { store } from './store/store'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </Provider>
-  </React.StrictMode>,
-);
-
+  </React.StrictMode>
+)
