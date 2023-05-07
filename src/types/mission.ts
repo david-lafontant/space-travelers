012e7349282c0063dae6944da6cd4aa4e5b5c6e8
david-lafontant <lafontant.david@gmail.com>
2023-05-07 15:32:56 -0600

    Setup new AMission type

---
+++ src/types/mission.ts
@@ -0,0 +1,10 @@
+export type AMission = {
+  mission_name: string
+  mission_id: string
+  manufacturers: string[]
+  payload_ids: string[]
+  wikipedia: string
+  website: string
+  twitter: null | string
+  description: string
+}
