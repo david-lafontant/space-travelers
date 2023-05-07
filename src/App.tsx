403d1a364a672e208db6fddfc794a78e5fd5f1e0
david-lafontant <lafontant.david@gmail.com>
2023-05-07 15:35:21 -0600

    Remove exact to comply with es6 requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
 import {
-  Route, createBrowserRouter, createRoutesFromElements, RouterProvider,
-} from 'react-router-dom';
-import Header from './components/header/Header';
-import {
-  Dragons, Missions, Profile, Rockets,
-} from './pages/index.js';
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+import Header from './components/header/Header'
+import { Dragons, Missions, Profile, Rockets } from './pages/index.js'
 
 const ROUTER = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Header />}>
       <Route index element={<Rockets />} />
-      <Route exact path="/missions" element={<Missions />} />
-      <Route exact path="/dragons" element={<Dragons />} />
-      <Route exact path="/profile" element={<Profile />} />
-    </Route>,
-  ),
-);
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/dragons" element={<Dragons />} />
+      <Route path="/profile" element={<Profile />} />
+    </Route>
+  )
+)
 function App() {
   return (
     <div className="App">
       <RouterProvider router={ROUTER} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
